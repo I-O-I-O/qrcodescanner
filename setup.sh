bab168c069632882b0308533ca037ecacc9f1864
Filippo <30697605+duiopio@users.noreply.github.com>
2025-05-12 21:29:26 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ cd qrscanner
 bin/pip install pyzbar
 bin/pip install opencv-python opencv-contrib-python
 bin/pip install selenium
+wget https://raw.githubusercontent.com/I-O-I-O/qrcodescanner/refs/heads/main/scanner.py
