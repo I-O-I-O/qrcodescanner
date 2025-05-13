045440ee11a8c14bfc309776ce8889fe130caf7c
Filippo <30697605+duiopio@users.noreply.github.com>
2025-05-13 15:04:44 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,18 @@
+echo "Creating folder..."
 mkdir qrscanner
+
+echo "Creating environment..."
 python3 -m venv qrscanner
 cd qrscanner
+
+echo "Installing pyzbar..."
 bin/pip install pyzbar
+
+echo "Installing opencv-python & opencv-contrib-python..."
 bin/pip install opencv-python opencv-contrib-python
+
+echo "Installing selenium..."
 bin/pip install selenium
+
+echo "Fetching scanner.py"
 wget https://raw.githubusercontent.com/I-O-I-O/qrcodescanner/refs/heads/main/scanner.py
