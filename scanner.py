fec6a4673a277fe63a1b7704f562c8e83162141e
Filippo <30697605+duiopio@users.noreply.github.com>
2025-05-13 17:07:15 +0000

    Find camera index

--- scanner.py
+++ scanner.py
@@ -9,10 +9,8 @@ from selenium.webdriver.firefox.service import Service
 
 options = Options()
 
-
 #options.add_argument('--kiosk')  # Or '--headless' if no display
 service = Service(executable_path='/usr/local/bin/geckodriver') 
-
 
 selenium = webdriver.Firefox(service=service, options=options)
 
@@ -25,30 +23,37 @@ pattern = r"^https://([a-zA-Z0-9-]+\.)*materialconnexion\.com(/.*)?$"
 
 # Set VideoCapture to 1 while testing on a laptop to use the external camera
 # TODO: Change to 0 when using the camera on the Raspberry Pi
-cap = cv2.VideoCapture(0)
+
+def find_camera():
+    for index in range(2):
+        cap = cv2.VideoCapture(index)
+        if cap.isOpened():
+            print(f"Camera found at index {index}")
+            return cap
+        cap.release()
+    return None
+
+cap = find_camera()
 
 # QR code detection Method
 detector = cv2.QRCodeDetector()
 
 while True:
 	# Returns a boolean value and the image
-	_, img = cap.read()
-	
+	ret, img = cap.read()
+	if not ret or img is None:
+		print("Failed to capture image from camera.")
+		continue
 	data, bbox, _ = detector.detectAndDecode(img)
-	
 	if data:
 		if re.match(pattern, data):
 			print("data found: ", data)
 			print("previous detection: ", lastData)
-				
 			if not data == lastData:
 				selenium.get(data)
 				selenium.fullscreen_window()
-		
 			lastData = data
-		
 	cv2.imshow("code detector", img)
-	
 	if(cv2.waitKey(1) == ord("q")):
 		break
 
