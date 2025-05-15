194434f10efb343c693b3183137af717da4e3e45
Filippo <30697605+duiopio@users.noreply.github.com>
2025-05-15 17:16:42 +0200

    Add firefox and geckodriver

--- setup.sh
+++ setup.sh
@@ -16,3 +16,21 @@ bin/pip install selenium
 
 echo "Fetching scanner.py"
 wget https://raw.githubusercontent.com/I-O-I-O/qrcodescanner/refs/heads/main/scanner.py
+
+cd 
+
+echo "Updating apt"
+sudo apt update
+
+echo "Installing firefox-esr"
+sudo apt install firefox-esr
+
+echo "Fetching geckodriver"
+wget https://github.com/mozilla/geckodriver/releases/download/v0.36.0/geckodriver-v0.36.0-linux-aarch64.tar.gz
+
+tar -xvzf geckodriver-v0.36.0-linux-aarch64.tar.gz
+
+chmod +x geckodriver
+
+sudo mv geckodriver /usr/local/bin/
+
